48161ca6b4c7a7ce15d8c111353f1239576c611d
Alexander <jayero@mail.ru>
2022-10-04 03:09:19 +0300

    solution2

---
+++ home_2.py
@@ -0,0 +1,27 @@
+# 2. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# Пример:  пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def factorial(num):
+    arr = []
+    count = 1
+    for i in range(1, num + 1):
+        count = count * i
+        arr.append(count)
+    return arr
+
+
+num = InputNumbers("Введите число: ")
+print(factorial(num))
